4581456a6a24919b355cb6023d20978a060a054b
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2022-03-04 20:51:06 +0200

    start of car class

--- CarDealership/Models/Car.cs
+++ CarDealership/Models/Car.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace CarDealership.Models
 {
-    internal class Car
+    public class Car
     {
+        public string id { get; set; }
+        public CarBrand carBrand { get; set; }
+        public double price { get; set; }
+        public DateTime manufDate { get; set; }
+        public DateTime saleDate { get; set; }
+        public double horsePower { get; set; }
+        public double kmDriven { get; set; }
+        public string imgDir { get; set; }
+        public double engineVolume { get; set; }
+        public double litres { get; set; }
+        public string info { get; set; }
+        public Car(string id, CarBrand carBrand, string manufDateStr, DateTime saleDate, double horsePower, double kmDriven, double engineVolume, double litres, string info)
+        {
+            this.id = id;
+            this.carBrand = carBrand;
+            this.manufDate = DateTime.Now;//placeholder
+            this.saleDate = saleDate;
+            this.horsePower = horsePower;
+            this.kmDriven = kmDriven;
+            this.engineVolume = engineVolume;
+            this.litres = litres;
+            this.info = info;
+        }
     }
 }
