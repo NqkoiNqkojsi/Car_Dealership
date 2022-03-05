a728d0dced8914620372263060ae53688a98a7c0
Stoil Stoilov <sstoilov2004@gmail.com>
2022-03-05 14:09:07 +0200

    Car context class CarBrand context class Database file created (isn't connected to server)

---
+++ CarDealership/Data/CarContext.cs
@@ -0,0 +1,33 @@
+﻿using CarDealership.Models;
+using System.Data.Entity;
+namespace CarDealership.Data
+{
+    public class CarContext : DbContext
+    {
+        /// <summary>
+        /// Connection String
+        /// </summary> 
+        private const string connectionString = null;//pull the connection string after making a connecting the database with sql server
+
+        /// <summary>
+        /// Cars Table
+        /// </summary>
+        public DbSet<Car> Cars { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ProductContext()
+        {
+            // Create the database automaticly
+            Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Connection string to Microsoft SQL Server
+        /// </summary> 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
---
+++ CarDealership/Data/SqlQuery_1.sql
@@ -0,0 +1,26 @@
+﻿CREATE DATABASE cardealership;
+
+USE cardealership;
+
+CREATE TABLE carbrand 
+(
+  idcar_brand INT NOT NULL IDENTITY,
+  brand VARCHAR(45) NOT NULL,
+  model VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idcar_brand)
+  );
+
+CREATE TABLE car (
+  id INT NOT NULL IDENTITY,
+  idcar_brand INT NOT NULL,
+  FOREIGN KEY (idcar_brand) REFERENCES carbrand(idcar_brand),
+  price FLOAT NOT NULL,
+  manufactureDate DATETIME NOT NULL,
+  horsepower FLOAT NOT NULL,
+  kmDriven FLOAT NULL,
+  imgDir VARCHAR(45) NULL,
+  engineVolume FLOAT NULL,
+  engineLitres FLOAT NULL,
+  additional_info VARCHAR(255) NULL,
+  PRIMARY KEY (id)
+  );
