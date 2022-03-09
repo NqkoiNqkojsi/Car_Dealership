363591a502f6a0c4cc308078ca80d71cbda05a21
Alex <daniedebel@gmail.com>
2022-03-10 00:04:46 +0200

    Main page experiments

--- CarDealership/Views/MainPage.xaml.cs
+++ CarDealership/Views/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Drawing;
+using System.Threading.Tasks;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,22 @@ namespace CarDealership
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Table.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+                await Task.Delay(60);
+                Table.Background = new SolidColorBrush(Windows.UI.Colors.Magenta);
+                await Task.Delay(60);
+            }
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Table.Background = new SolidColorBrush(Windows.UI.Colors.WhiteSmoke);
         }
     }
 }
