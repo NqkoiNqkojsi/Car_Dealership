0459c8e49060eb1fb22a920d70f9e77d0a042ced
Stelian Grozev <stelyo_2004@abv.bg>
2022-03-12 18:55:21 +0200

    made the front of car showcase and started the main view

--- CarDealership/Controllers/CustomerController.cs
+++ CarDealership/Controllers/CustomerController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CarDealership.Models;
 using System.Security.Cryptography;
+using System.Web;
 
 namespace CarDealership.Controllers
 {
---
+++ CarDealership/Views/CarShowCase.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using CarDealership.Controllers;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CarDealership.Views
+{
+    public sealed partial class CarShowCase : UserControl
+    {
+        public string id { get; set; }
+        private void MakeContent()
+        {
+            List<string> components=new List<string>();
+
+        }
+        public CarShowCase(string id)
+        {
+            this.InitializeComponent();
+            this.id = id;
+            MakeContent();
+        }
+        private void OpenCarPage()
+        {
+            
+        }
+    }
+}
