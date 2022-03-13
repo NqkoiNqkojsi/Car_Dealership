4a13b0f197a72d32baf52d2191762afd1d93ea01
Stoyan Kutsarov <devorer@abv.bg>
2022-03-13 14:16:09 +0200

    Added Password Recovery

--- CarDealership/Controllers/CustomerController.cs
+++ CarDealership/Controllers/CustomerController.cs
@@ -32,6 +32,53 @@ namespace CarDealership.Controllers
         }
 
         /// <summary>
+        /// Random Number between 2 Endpoints
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        /// <summary>
+        /// Returns random jumble of letters with specified size and upper/lowercase
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="lowerCase"></param>
+        /// <returns></returns>
+        public static string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Makes a random password out of numbers and letters
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static string RandomPassword(int size = 0)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(RandomString(4, true));
+            builder.Append(RandomNumber(1000, 9999));
+            builder.Append(RandomString(2, false));
+            return builder.ToString();
+        }
+
+        /// <summary>
         /// Checks Email Validity
         /// </summary>
         /// <param name="email"></param>
@@ -128,7 +175,22 @@ namespace CarDealership.Controllers
         {
             bool ValidInfo = customers.Any(c => c.name== userName && c.Password==password);
             if (ValidInfo) customers.Where(c => c.name == userName && c.Password == password).FirstOrDefault().isLoggedIn=true;
+            Console.WriteLine("You are logged in!");
         }
 
+        /// <summary>
+        /// Sends you an email to recover your password
+        /// </summary>
+        /// <param name="email"></param>
+        public static void ForgottenPasswords(string email)
+        {
+            bool trueMail = customers.Any(c => c.email == email);
+            if (trueMail)
+            {
+                string newPass = RandomPassword();
+                CustomerController.customers.Where(c => c.email == email).FirstOrDefault().Password = newPass;
+                SendEmail(email, "Password Recovery", $"Your new password is {newPass}, log in to your account and update it to whatever you want.");
+            }
+        }
     }
 }
