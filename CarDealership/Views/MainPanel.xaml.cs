8bb204dd19ae989123833ec07c6dc12b2c3e1477
StanimirRaykov <mirkokr2004@gmail.com>
2022-03-15 20:21:43 +0200

    Sign Up Page Updates

--- CarDealership/Views/MainPanel.xaml.cs
+++ CarDealership/Views/MainPanel.xaml.cs
@@ -23,5 +23,15 @@ namespace CarDealership.Views
         {
             this.InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
