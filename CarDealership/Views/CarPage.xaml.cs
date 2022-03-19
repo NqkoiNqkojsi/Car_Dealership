94c387c449f71a89afce1644dacc8a4dab68362e
Stelian Grozev <tanya_dzh@abv.bg>
2022-03-19 16:23:24 +0200

    made showing of information in carPage

--- CarDealership/Views/CarPage.xaml.cs
+++ CarDealership/Views/CarPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
 using CarDealership.Controllers;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -21,10 +22,19 @@ namespace CarDealership.Views
     public sealed partial class CarPage : UserControl
     {
         public string id { get; set; }
+        public List<BitmapImage> images=new List<BitmapImage>();
         private void MakeContent(string id)
         {
             Dictionary<string, string> info = CarController.IDtoCarInfo(id);
             Title.Text = info["brand"] + " " + info["model"];
+            textBlockDate.Text = "Made in"+info["date"];
+            textBlockPrice.Text = "Price: " + info["price"]+"lv";
+            textBlockKmDriven.Text = "Driven:" + info["km"] + "km";
+            textBlockHorsePower.Text = "Power:" + info["horsePower"];
+            textBlockLitres.Text = "Volume:" + info["engineVolume"]+"l";
+            textBlockInfo.Text = info["addInfo"];
+            textBlockName.Text = info["seller"];
+            textBlockPhone.Text = info["sellerPhone"];
             //Image.Source=;
         }
         public CarPage(string id)
@@ -32,6 +42,7 @@ namespace CarDealership.Views
             this.InitializeComponent();
             this.id = id;
             MakeContent(id);
+            
         }
     }
 }
